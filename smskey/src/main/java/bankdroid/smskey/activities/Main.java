0c8cef99d5737544cac4328ca06a3a7a0fea3275
Paweł Majkutewicz <pmajkutewicz@gmail.com>
2017-11-02 23:58:30 +0100

    Ask for READ_SMS permission before using "View last code"

--- smskey/src/main/java/bankdroid/smskey/activities/Main.java
+++ smskey/src/main/java/bankdroid/smskey/activities/Main.java
@@ -1,17 +1,22 @@
 package bankdroid.smskey.activities;
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.database.Cursor;
-import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.View;
 import bankdroid.smskey.BankManager;
+import bankdroid.smskey.BuildConfig;
 import bankdroid.smskey.Codes;
 import bankdroid.smskey.Eula;
 import bankdroid.smskey.Message;
@@ -20,8 +25,12 @@ import bankdroid.smskey.SMSCheckerTask;
 import bankdroid.smskey.SMSCheckerTask.OnFinishListener;
 import bankdroid.smskey.bank.Bank;
 
+import java.util.List;
+
 public class Main extends MenuActivity implements Codes {
 	private final static int DIALOG_NOCODE = 567;
+	private static final int PERMISSION_REQUEST_CODE = 123;
+	private static final int IGNORED_REQUEST_CODE = 0;
 	private ProgressDialog progressDialog;
 
 	@Override
@@ -31,6 +40,18 @@ public class Main extends MenuActivity implements Codes {
 		Eula.show(this);
 
 		setContentView(R.layout.main);
+
+		if(ContextCompat.checkSelfPermission(getBaseContext(), Manifest.permission.READ_SMS) == PackageManager.PERMISSION_DENIED) {
+			ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_SMS}, IGNORED_REQUEST_CODE);
+		}
+
+		if (BuildConfig.DEBUG) {
+			Intent smsRecvIntent = new Intent("android.provider.Telephony.SMS_RECEIVED");
+			List<ResolveInfo> infos = getPackageManager().queryBroadcastReceivers(smsRecvIntent, 0);
+			for (ResolveInfo info : infos) {
+				Log.d(TAG, "Receiver: " + info.activityInfo.name + ", priority=" + info.priority);
+			}
+		}
 
 		//find out whether there is any supported bank in the country
 		final TelephonyManager telephonyManager = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
@@ -54,6 +75,24 @@ public class Main extends MenuActivity implements Codes {
 	}
 
 	public void onViewLastCode(final View v) {
+		if(ContextCompat.checkSelfPermission(getBaseContext(), Manifest.permission.READ_SMS) == PackageManager.PERMISSION_DENIED) {
+			ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_SMS}, PERMISSION_REQUEST_CODE);
+		} else {
+			showSmsOTP();
+		}
+	}
+
+	@Override
+	public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+		if (PERMISSION_REQUEST_CODE == requestCode &&
+			grantResults.length == 1 &&
+			grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+			Manifest.permission.READ_SMS.equals(permissions[0])) {
+			showSmsOTP();
+		}
+	}
+
+	private void showSmsOTP() {
 		final Message message = BankManager.getLastMessage(getApplicationContext());
 		if (message == null) {
 			//if there is no processed code yet, check the SMS inbox for archive.
